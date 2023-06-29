1d68fe76f8e70e7ce082cd20e89f4c0e995e7bce
fmora122 <pfmoras@gmail.com>
2023-06-28 20:37:46 -0600

    complete

--- Fabricio_MoraSalazar/src/fabricio_morasalazar/Fabricio_MoraSalazar.java
+++ Fabricio_MoraSalazar/src/fabricio_morasalazar/Fabricio_MoraSalazar.java
@@ -4,6 +4,8 @@
  */
 package fabricio_morasalazar;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author fmora
@@ -15,6 +17,65 @@ public class Fabricio_MoraSalazar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        invoiceCollector myInvoice = new invoiceCollector();
+        double debt = 0;
+        int option = 1;
+        while (option != 0) {
+            System.out.println("Main Menu - Select an option");
+            System.out.println("1) To add an invoice.");
+            System.out.println("2) Calculate Debt and Payment Plan.");
+            System.out.println("3) Forecast next 6 Months");
+            System.out.println("0) To exit.");
+            System.out.println("");
+            option = Integer.parseInt(JOptionPane.showInputDialog("Type your selection"));
+            
+            
+            if (option == 1) {
+                // *
+                // Here we collect data and estimate the debt value by summing invoices in pending status
+                // *
+                System.out.println("New Invoice Menu");
+                System.out.println("Please add the invoice data");
+                System.out.println("");
+                myInvoice.setCustomerName(JOptionPane.showInputDialog("Customer Full Name"));
+                //
+                myInvoice.setCustomerCedula(Integer.parseInt(JOptionPane.showInputDialog("Customer Cedula")));
+                myInvoice.setInvoiceID(Integer.parseInt(JOptionPane.showInputDialog("Invoice ID")));
+                myInvoice.setInvoiceCost(Double.parseDouble(JOptionPane.showInputDialog("Invoice Cost")));
+                myInvoice.setInvoiceMonth(Integer.parseInt(JOptionPane.showInputDialog("Invoice Month")));
+                myInvoice.setInvoiceYear(Integer.parseInt(JOptionPane.showInputDialog("Invoice Year")));
+                myInvoice.setInvoiceStatus(Integer.parseInt(JOptionPane.showInputDialog("Status: 1 for Payed | 2 for Pending")));
+                
+                if (myInvoice.getInvoiceStatus() == 2) {
+                    debt = debt + myInvoice.getInvoiceCost();
+                }
+                
+            } else if (option == 2) {
+                System.out.println("*Customer Name:");
+                System.out.println(myInvoice.getCustomerName());
+                System.out.println("*Total Debt:");
+                System.out.println(debt);
+                System.out.println("*Total Debt + interests:");
+                System.out.println(debt+(debt*0.05));
+                System.out.println("*Monthly Payment");
+                System.out.println((debt+(debt*0.05))/6);
+                System.out.println("");
+                
+            } else if (option == 3) {
+                System.out.println("Invoice Forecast");
+                System.out.println("");
+                for (int month = 1; month <= 6; month ++) {
+                    System.out.println("Payment #"+month);
+                    System.out.println("Amount: "+myInvoice.getInvoiceCost());
+                    //int currentMonth = myInvoice.getInvoiceMonth+
+                    System.out.println("Month: "+(myInvoice.getInvoiceMonth()+month));
+                    System.out.println("Year: "+myInvoice.getInvoiceYear());
+                    System.out.println("");
+                    
+                }
+            }
+                    
+        }
     }
     
 }
---
+++ Fabricio_MoraSalazar/src/fabricio_morasalazar/invoiceCollector.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fabricio_morasalazar;
+
+/**
+ *
+ * @author fmora
+ */
+public class invoiceCollector {
+    private String customerName = "";
+    private int customerCedula = 0;
+    private int invoiceID = 0;
+    private double invoiceCost = 0.0; 
+    private int invoiceYear = 0;
+    private int invoiceMonth = 0;
+    private int invoiceStatus = 0;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerCedula() {
+        return customerCedula;
+    }
+
+    public void setCustomerCedula(int customerCedula) {
+        this.customerCedula = customerCedula;
+    }
+
+    public int getInvoiceID() {
+        return invoiceID;
+    }
+
+    public void setInvoiceID(int invoiceID) {
+        this.invoiceID = invoiceID;
+    }
+
+    public double getInvoiceCost() {
+        return invoiceCost;
+    }
+
+    public void setInvoiceCost(double invoiceCost) {
+        this.invoiceCost = invoiceCost;
+    }
+
+    public int getInvoiceYear() {
+        return invoiceYear;
+    }
+
+    public void setInvoiceYear(int invoiceYear) {
+        this.invoiceYear = invoiceYear;
+    }
+
+    public int getInvoiceMonth() {
+        return invoiceMonth;
+    }
+
+    public void setInvoiceMonth(int invoiceMonth) {
+        this.invoiceMonth = invoiceMonth;
+    }
+
+    public int getInvoiceStatus() {
+        return invoiceStatus;
+    }
+
+    public void setInvoiceStatus(int invoiceStatus) {
+        this.invoiceStatus = invoiceStatus;
+    }
+    
+    
+    
+    
+}
